25070dac8f130aadf6d51dc6f1efbb80795e3cf6
David Viteri <davidteri91@gmail.com>
2015-06-16 11:32:01 +0200

    Sony AOSP blobs: released 2015-06-15
    From: http://developer.sonymobile.com/knowledge-base/open-source/open-devices/latest-updates/

--- qcom/prebuilt/qcom-vendor.mk
+++ qcom/prebuilt/qcom-vendor.mk
@@ -1,2 +1,2 @@
-$(call inherit-product, vendor/qcom/prebuilt/qcom-partial.mk)
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-32bit.mk)
 $(call inherit-product-if-exists, vendor/qcom/proprietary/qcom-firmware.mk)
